c2ca18e25f22b904da0072c3f44112e9d994afbb
ShokhrookhR <shokh7997@gmail.com>
2022-09-16 18:00:20 +0500

    Modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import Modal from './components/Modal';
 import './index.scss';
 
 function App() {
+  const [isVisible, setisVisible] = React.useState(false);
+  const changeVisibilityModal = () => {
+    setisVisible(!isVisible);
+  };
   return (
     <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div>
+      <button className="open-modal-btn" onClick={changeVisibilityModal}>
+        ✨ Открыть окно
+      </button>
+      {isVisible && <Modal changeVisibilityModal={changeVisibilityModal} />}
     </div>
   );
 }
