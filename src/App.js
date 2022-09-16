6006f9dce8b146181b5bacede6436d40a9950925
ShokhrookhR <shokh7997@gmail.com>
2022-09-16 19:16:50 +0500

    Finish

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import './index.scss';
 
 const questions = [
@@ -22,37 +24,59 @@ const questions = [
   },
 ];
 
-function Result() {
+function Result({ onClickRestart, correct }) {
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="img" />
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <button onClick={onClickRestart}>Попробовать снова</button>
     </div>
   );
 }
 
-function Game() {
+function Game({ question, onClickVariant, step }) {
+  const percentage = Math.round((step / questions.length) * 100);
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((v, i) => (
+          <li onClick={() => onClickVariant(i)} key={v}>
+            {v}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
+  const question = questions[step];
+
+  const onClickVariant = (index) => {
+    setStep(step + 1);
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  };
+  const onClickRestart = () => {
+    setStep(0);
+    setCorrect(0);
+  };
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {step !== questions.length ? (
+        <Game question={question} onClickVariant={onClickVariant} step={step} />
+      ) : (
+        <Result onClickRestart={onClickRestart} correct={correct} />
+      )}
     </div>
   );
 }
