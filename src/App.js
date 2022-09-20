96af4567b1acf8cab7ba17cbd3bd4ae1925fab08
ShokhrookhR <shokh7997@gmail.com>
2022-09-21 00:31:34 +0500

    Курс валют

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+import axios from 'axios';
 import React from 'react';
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [rates, setRates] = React.useState({});
+  const [fromCurrency, setFromCurrency] = React.useState('RUB');
+  const [toCurrency, setToCurrency] = React.useState('USD');
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(0);
+
+  React.useEffect(() => {
+    axios
+      .get('https://cdn.cur.su/api/latest.json')
+      .then((res) => {
+        setRates(res.data.rates);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const onChangeFromPrice = (value) => {
+    const price = value / rates[fromCurrency];
+    const result = price * rates[toCurrency];
+    setFromPrice(value);
+    setToPrice(result);
+  };
+  const onChangeToPrice = (value) => {
+    const price = value / rates[toCurrency];
+    const result = price * rates[fromCurrency];
+    setFromPrice(result);
+    setToPrice(value);
+  };
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
